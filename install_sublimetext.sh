6618d1264682a9f5a459bb2910986855eba3c8a3
tiena2cva <tiena2cva@gmail.com>
2017-05-07 23:28:27 +0700

    Update install sublime_text

--- install_sublimetext.sh
+++ install_sublimetext.sh
@@ -1,6 +1,9 @@
 cd ~
 mkdir Tools
 cd Tools
-git clone https://github.com/tiena2cva/sublimetext3
+git clone https://github.com/tiena2cva/sublime_text_3
 
 sudo cp sublime_text.desktop /usr/share/applications/
+
+cd ~/.config
+ln -s ~/Tools/sublime_text_3/.config/sublime-text-3
