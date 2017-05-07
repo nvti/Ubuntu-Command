bbd941724d372daddccac777d5eb69246d8acbcd
tiena2cva <tiena2cva@gmail.com>
2017-05-07 22:40:54 +0700

    test lenh ln

--- test.cpp
+++ test.cpp
@@ -1,1 +1,20 @@
-../test.cpp
+aasafda
+for (int i = 0; i < aasafdaa
+	#ifndef daf
+	#define daf
+	aasafdaafa
+	for (int i = 0; i < aasafdaafaf
+		afdadfad
+		fadfad
+		afdadfadf
+		for (int i = 0; i < count; ++i)
+		{
+			/* code */
+		}; ++i)
+	 {
+	 	/* code */
+	 } value
+	#endif; ++i)
+{
+	/* code */
+}
